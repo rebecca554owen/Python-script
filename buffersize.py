2b0bf4cab55068fa1cf2f4ee3899fe06d10b96b5
rebecca554owen <rebecca554owen@gmail.com>
2020-11-29 20:32:09 +0800

    1

--- buffersize.py
+++ buffersize.py
@@ -2,13 +2,13 @@ import time
 
 begin = time.time()
 buffersize = 2 ** 30  # 设置缓冲区，加速读取。
-with open("D:/Update/file.txt") as f:
+with open("file.txt") as f:
     while True:
         lines_buffer = f.readlines(buffersize)
         if not lines_buffer:
             break
         for line in lines_buffer:
-            if "836120472" in line:
+            if "key word" in line:
                 print(line)
     end = time.time()
     print('time is %d seconds ' % (end - begin))
